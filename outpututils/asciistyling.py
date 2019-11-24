830811471765f62cabfcaa1056ca7ffdf73f5eb1
András Németh <sunarchx@gmail.com>
2019-11-24 17:16:21 +0100

    Add ASCII styling classes

---
+++ outpututils/asciistyling.py
@@ -0,0 +1,137 @@
+
+# style ########################################################################
+
+class AsciiStyle():
+
+    @staticmethod
+    def bold(arg_printable):
+        return "\x1b[1m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def faint(arg_printable):
+        return "\x1b[2m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def italic(arg_printable):
+        return "\x1b[3m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def underlined(arg_printable):
+        return "\x1b[4m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def blink(arg_printable):
+        return "\x1b[5m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def image_negative(arg_printable):
+        return "\x1b[7m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def framed(arg_printable):
+        return "\x1b[51m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def encircled(arg_printable):
+        return "\x1b[52m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def overlined(arg_printable):
+        return "\x1b[53m{0}\x1b[0m".format(arg_printable)
+
+# font #########################################################################
+
+class AsciiFont():
+
+    @staticmethod
+    def primary(arg_printable):
+        return "\x1b[10m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def alternate(arg_printable, arg_alternate_font_no):
+        inserted_no = "1"
+        if int(arg_alternate_font_no) in range(1, 10): # 0 < x < 10 #
+            inserted_no = str(arg_alternate_font_no)
+        return "\x1b[1{0}m{1}\x1b[0m".format(inserted_no, arg_printable)
+
+# color ########################################################################
+
+class AsciiColor():
+
+    @staticmethod
+    def black(arg_printable):
+        return "\x1b[30m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def red(arg_printable):
+        return "\x1b[31m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def green(arg_printable):
+        return "\x1b[32m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def yellow(arg_printable):
+        return "\x1b[33m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def blue(arg_printable):
+        return "\x1b[34m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def magenta(arg_printable):
+        return "\x1b[35m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def cyan(arg_printable):
+        return "\x1b[36m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def white(arg_printable):
+        return "\x1b[37m{0}\x1b[0m".format(arg_printable)
+# Output styling with ASCII escape sequences
+# Version 1.0
+
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+
+# Python utilites
+# https://github.com/sunarch/libmonty
+# 'utils' folder
+
+# background ###################################################################
+
+class AsciiBackground():
+
+    @staticmethod
+    def black(arg_printable):
+        return "\x1b[40m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def red(arg_printable):
+        return "\x1b[41m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def green(arg_printable):
+        return "\x1b[42m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def yellow(arg_printable):
+        return "\x1b[43m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def blue(arg_printable):
+        return "\x1b[44m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def magenta(arg_printable):
+        return "\x1b[45m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def cyan(arg_printable):
+        return "\x1b[46m{0}\x1b[0m".format(arg_printable)
+
+    @staticmethod
+    def white(arg_printable):
+        return "\x1b[47m{0}\x1b[0m".format(arg_printable)
+
+# END ##########################################################################
