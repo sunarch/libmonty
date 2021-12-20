190984c468458ceb0664bd7cde3bdeff640a05cc
András Németh <sunarch@protonmail.com>
2021-12-20 02:04:40 +0100

    Update file headers

--- misc/email/email-check.py
+++ misc/email/email-check.py
@@ -1,4 +1,11 @@
-from email_validator import validate_email, EmailNotValidError, EmailSyntaxError, EmailUndeliverableError 
+#!/usr/bin/env python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from email_validator import validate_email, EmailNotValidError, EmailSyntaxError, EmailUndeliverableError
 
 ls_emails = [
    
--- src/libmonty/outpututils/decorators.py
+++ src/libmonty/outpututils/decorators.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
-#
+#!/usr/bin/env python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 
 # Output decorators with ASCII escape sequences
 # Version 1.1
