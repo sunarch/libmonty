095897ad2df0bef739a878e034aa3f65be02b364
András Németh <sunarch@protonmail.com>
2023-06-13 13:03:22 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+pip install -r requirements-dev.txt
+python -m build
