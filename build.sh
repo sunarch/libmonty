d7614a1e78de94bcd207d07b77cfa24f348f16d2
András Németh <sunarch@protonmail.com>
2023-07-07 20:25:48 +0200

    Remove editor config lines from files

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-# -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
