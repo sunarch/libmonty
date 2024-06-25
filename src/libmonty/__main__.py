0d826abc27d1f43dab905e366de195d848d8fe5f
András Németh <sunarch@protonmail.com>
2024-06-25 16:42:59 +0200

    Update requirements

--- src/libmonty/__main__.py
+++ src/libmonty/__main__.py
@@ -2,16 +2,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-"""Main
-"""
+"""Main"""
 
 # imports: library
 from argparse import ArgumentParser
 import sys
 
 # imports: dependencies
-from libmonty_logging.config.file_and_stream.v1 import config as logging_config
-import libmonty_logging.helper as logging_helper
+import libmonty_logging
 import libmonty_logging.message as logging_message
 
 # imports: project
@@ -21,9 +19,10 @@ from libmonty import version
 def main() -> None:
     """Main"""
 
-    logging_helper.apply_config(version.PROGRAM_NAME,
-                                version.__version__,
-                                logging_config)
+    libmonty_logging.apply_default_console_and_file(
+        version.PROGRAM_NAME,
+        version.__version__
+    )
 
     logging_message.program_header(version.PROGRAM_NAME)
 
